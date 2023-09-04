5ceeac37d0b07a07e0dd7fc5da55cd14bd72ea2e
atonya-bravin <bravinatonya.brani@gmail.com>
2023-09-04 13:47:51 +0300

    creation of the redis client with all the functions that facilitate it's functionality

---
+++ utils/redis.js
@@ -0,0 +1,33 @@
+const redis = require('redis');
+
+class RedisClient {
+  constructor(options) {
+    this.redisClient = new redis.createClient(options);
+
+    this.redisClient.on('error', (err) => {
+      console.error(err);
+    });
+  }
+
+  isAlive() {
+    return this.redisClient.status === 'ready';
+  }
+
+  async get(key) {
+    const reply = await this.redisClient.get(key);
+    return reply;
+  }
+
+  async set(key, value, duration) {
+    await this.redisClient.setex(key, duration, value);
+  }
+
+  async del(key) {
+    await this.redisClient.del(key);
+  }
+}
+
+export default new RedisClient({
+  host: 'localhost',
+  port: 6379,
+});
