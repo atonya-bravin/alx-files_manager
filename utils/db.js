a92371e0d354bdbe2101aadb97866a02071d908c
atonya-bravin <bravinatonya.brani@gmail.com>
2023-09-05 21:40:50 +0300

    connection to the db

---
+++ utils/db.js
@@ -0,0 +1,30 @@
+const { MongoClient } = require('mongodb');
+
+class DBClient {
+  constructor() {
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const database = process.env.DB_DATABASE || 'files_manager';
+
+    // Connection URL for MongoDB
+    const url = `mongodb://${host}:${port}/${database}`;
+
+    // Create a new MongoClient
+    this.client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
+
+    this.isConnected = false;
+    this.client.connect((err) => {
+      if (!err) {
+        this.isConnected = true;
+        this.db = this.client.db(database);
+      }
+    });
+  }
+
+  isAlive() {
+    return this.isConnected;
+  }
+}
+
+export default new DBClient();
+export { DBClient as dbClient };
